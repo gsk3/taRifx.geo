54412b6439924efe297abad0d69ecaea581b626e
Ari B. Friedman <abfriedman@gmail.com>
2012-09-14 09:12:34 -0400

    -Added IDs and IDs<- generic and methods for SpatialPolygonsDataFrame

--- R/R-GIS.R
+++ R/R-GIS.R
@@ -503,10 +503,11 @@ SPDFtoPointsDF <- function(SPDF) {
 	return(cbind(SPDF@data,coords))
 }
 
-#' Get polygon IDs
+#' Get sp feature IDs
 #' @aliases IDs IDs.default IDs.SpatialPolygonsDataFrame
 #' @param x The object to get the IDs from
 #' @param \dots Pass-alongs
+#' @author Ari B. Friedman
 #' @rdname IDs
 IDs <- function(x,...) {
   UseMethod("IDs",x)
@@ -521,11 +522,23 @@ IDs.default <- function(x,...) {
 #' @S3method IDs SpatialPolygonsDataFrame
 #' @rdname IDs
 IDs.SpatialPolygonsDataFrame <- function(x,...) {
-  vapply(slot(st1, "polygons"), function(x) slot(x, "ID"), "")
+  vapply(slot(x, "polygons"), function(x) slot(x, "ID"), "")
 }
-#! THis needs to work
-`IDs.SpatialPolygonsDataFrame<-` <- function(x,...) {
-  spChFIDs()
+
+#' Assign sp feature IDs
+#' @aliases IDs<- IDs.default<-
+#' @param x The object to assign to
+#' @param value The character vector to assign to the IDs
+#' @author Ari B. Friedman
+#' @rdname IDs<-
+"IDs<-" <- function( x, value ) {
+  UseMethod("IDs<-",x)
+}
+#' @method IDs<- SpatialPolygonsDataFrame
+#' @S3method IDs<- SpatialPolygonsDataFrame
+#' @rdname IDs<-
+"IDs<-.SpatialPolygonsDataFrame" <- function( x, value) {
+  spChFIDs(x,value)
 }
 
 #' rbind SpatialPolygonsDataFrames together, fixing IDs if duplicated
@@ -536,19 +549,29 @@ IDs.SpatialPolygonsDataFrame <- function(x,...) {
 #' @method rbind.SpatialPolygonsDataFrame
 #' @export rbind.SpatialPolygonsDataFrame
 rbind.SpatialPolygonsDataFrame <- function(..., fix.duplicated.IDs=TRUE) {
-    dots = list(...)
-    names(dots) <- NULL
-    # Check IDs for duplicates and fix if indicated
-    IDs.list <- lapply(dots,IDs)
-    if( any(duplicated(unlist(IDs.list))) ) {
-      if(fix.duplicated.IDs) {
-        
-      } else {
-        stop("There are duplicated IDs, and fix.duplicated.IDs is not TRUE.")
+  dots <- as.list(substitute(list(...)))[-1L]
+  dots_names <- as.character(dots) # store names of objects passed in to ... so that we can use them to create unique IDs later on
+  dots <- lapply(dots,eval)
+  names(dots) <- NULL
+  # Check IDs for duplicates and fix if indicated
+  IDs_list <- lapply(dots,IDs)
+  dups.sel <- duplicated(unlist(IDs_list))
+  if( any(dups.sel) ) {
+    if(fix.duplicated.IDs) {
+      dups <- unique(unlist(IDs_list)[dups.sel])
+      # Function that takes a SPDF, a string to prepend to the badID, and a character vector of bad IDs
+      fixIDs <- function( x, prefix, badIDs ) {
+        sel <-  IDs(x) %in% badIDs
+        IDs(x)[sel] <- paste( prefix, IDs(x)[sel], sep="." )
+        x
       }
+      dots <- mapply(FUN=fixIDs , dots, dots_names, MoreArgs=list(badIDs=dups) )
+    } else {
+      stop("There are duplicated IDs, and fix.duplicated.IDs is not TRUE.")
     }
-    # One call to bind them all
-    pl = do.call("rbind", lapply(dots, function(x) as(x, "SpatialPolygons")))
-    df = do.call("rbind", lapply(dots, function(x) x@data))
-    SpatialPolygonsDataFrame(pl, df)
+  }
+  # One call to bind them all
+  pl = do.call("rbind", lapply(dots, function(x) as(x, "SpatialPolygons")))
+  df = do.call("rbind", lapply(dots, function(x) x@data))
+  SpatialPolygonsDataFrame(pl, df)
 }
