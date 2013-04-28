ad2d62ee155d96a8d91304bdc623f54854caff04
arib <arib@Mobile1.(none)>
2013-04-27 22:40:53 -0400

    - Added example polygon outside the points

--- data/tgExamples.R
+++ data/tgExamples.R
@@ -4,9 +4,10 @@ require(sp)
 Srs1 = Polygons(list(Polygon(cbind(c(2,4,4,1,2),c(2,3,5,4,2)))), "s1")
 Srs2 = Polygons(list(Polygon(cbind(c(5,4,2,5),c(2,3,2,2)))), "s2")
 Srs3 = Polygons(list(Polygon(cbind(c(5,1,2,5,5),c(0,0,2,2,0)))), "s3")
+Srs4 = Polygons(list(Polygon(cbind(c(5,3,1,5),c(0,-2,0,0)))),"s4")
 
-polySP <- SpatialPolygonsDataFrame( SpatialPolygons(list(Srs1,Srs2,Srs3)), 
-                                  data.frame( z=1:3, row.names=c("s1","s2","s3") ) )
+polySP <- SpatialPolygonsDataFrame( SpatialPolygons(list(Srs1,Srs2,Srs3,Srs4)), 
+                                  data.frame( z=1:4, row.names=c("s1","s2","s3","s4") ) )
 
 set.seed(123)
 pointSP <- SpatialPointsDataFrame( 
